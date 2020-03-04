4cd56d5dc3a5ef52f0abd3a5d731a37ff0b21fe6
Huang Po-Hsun <fh831.cp9gw@gmail.com>
2020-03-04 16:55:43 +0800

    sign Ip組件添加拼圖驗證碼，調整彈出框的邏輯

--- src/application/sign/page/Login.vue
+++ src/application/sign/page/Login.vue
@@ -5,6 +5,7 @@
       <v-text-field
         v-model="name"
         :error-messages="nameErrors"
+        :success-messages="nameSuccess"
         label="Username / E-mail"
         required
         @input="$v.name.$touch()"
@@ -16,6 +17,7 @@
         v-model="password"
         :type="seePwd"
         :error-messages="passwordErrors"
+        :success-messages="passwordSuccess"
         label="Password"
         required
         @input="$v.password.$touch()"
@@ -32,26 +34,60 @@
         @change="$v.checkbox.$touch()"
         @blur="$v.checkbox.$touch()"
       ></v-checkbox>
+      <!-- verity -->
+      <v-dialog v-model="dialog" width="500" :disabled="openDialog">
+        <template v-slot:activator="{ on }">
+          <v-btn class="mr-4" color="primary" v-on="on" @click="submit"
+            >sign in</v-btn
+          >
+        </template>
+        <v-card>
+          <v-card-title class="headline grey lighten-2" primary-title>
+            verity
+          </v-card-title>
 
-      <v-btn class="mr-4" color="primary" @click="submit">sign in</v-btn>
-      <v-btn @click="clear">clear</v-btn><br><br>
+          <v-card-text>
+            <div
+              style="display: flex; justify-content: center; padding: 1rem; background: white;"
+            >
+              <slide-verify
+                :l="42"
+                :r="10"
+                :w="310"
+                :h="155"
+                slider-text="Swipe right"
+                @success="onSuccess"
+                @fail="onFail"
+                @refresh="onRefresh"
+              ></slide-verify>
+            </div>
+          </v-card-text>
+
+          <v-divider></v-divider>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="primary" text @click="dialog = false">
+              Cancel
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+      <v-btn @click="clear">clear</v-btn><br /><br />
       No account?To <a href="sign#/Register">sign up</a>.
     </form>
   </div>
 </template>
 <script>
 import { validationMixin } from "vuelidate";
-import {
-  required,
-  minLength,
-  email
-} from "vuelidate/lib/validators";
+import { required, minLength, email } from "vuelidate/lib/validators";
+import axios from "axios";
 
 export default {
   mixins: [validationMixin],
 
   validations: {
-    name: { required},
+    name: { required },
     password: { required, minLength: minLength(6) },
     checkbox: {
       checked(val) {
@@ -59,13 +95,24 @@ export default {
       }
     }
   },
-
+  data() {
+    return {
+      msg: ""
+    };
+  },
   data: () => ({
     name: "",
     password: "",
     checkbox: true,
     eye: "mdi-eye-off",
-    seePwd: "password"
+    seePwd: "password",
+    dialog: false,
+    nameError: true,
+    passwordError: true,
+    // checkboxError: false,
+    loginURL: "",
+    loginSuccess: false,
+    openDialog: true
   }),
 
   computed: {
@@ -73,12 +120,16 @@ export default {
       const errors = [];
       if (!this.$v.checkbox.$dirty) return errors;
       !this.$v.checkbox.checked && errors.push("You must agree to continue!");
+      // this.checkboxError = true;
+      this.openDialog = true;
       return errors;
     },
     nameErrors() {
       const errors = [];
       if (!this.$v.name.$dirty) return errors;
       !this.$v.name.required && errors.push("Username / E-mail is required.");
+      this.nameError = true;
+      this.openDialog = true;
       return errors;
     },
     passwordErrors() {
@@ -87,29 +138,89 @@ export default {
       !this.$v.password.minLength &&
         errors.push("Password must have at least 6 letters.");
       !this.$v.password.required && errors.push("Password is required.");
+      this.passwordError = true;
+      this.openDialog = true;
       return errors;
-    }
+    },
+    nameSuccess() {
+      if (this.name !== "") {
+        this.nameError = false;
+        console.log("nameSuccess");
+      }
+    },
+    passwordSuccess() {
+      if (this.password !== "" && this.$v.password.minLength) {
+        this.passwordError = false;
+        console.log("passwordSuccess");
+      }
+    },
+    // checkboxSuccess() {
+    //   if (this.checkbox === true && this.$v.checkbox.checked) {
+    //     this.checkboxError = false;
+    //     console.log("checkboxSuccess");
+    //   }
+    // }
   },
 
   methods: {
     submit() {
+      this.openDialog = true;
       this.$v.$touch();
+      console.log("this.nameError: " + this.nameError);
+      console.log("this.passwordError: " + this.passwordError);
+      // console.log("this.checkboxError: " + this.checkboxError);
+      console.log("this.checkbox: " + this.checkbox);
+      if (
+        this.nameError === false &&
+        this.passwordError === false &&
+        this.checkbox === true
+      ) {
+        // submit the login request
+        /*axios({
+          method: "post",
+          url: this.loginURL,
+          data: { username: this.name, password: this.password }
+        })
+          .then(response => {
+            console.log(response);
+            console.log(response.data);
+            this.loginSuccess = response.data;
+          })
+          .catch(error => {
+            console.log(error);
+          });*/
+        this.openDialog = false;
+        this.dialog = true;
+      }
     },
     clear() {
       this.$v.$reset();
       this.name = "";
       this.password = "";
-      this.checkbox = false;
+      this.checkbox = true;
+      this.nameError = true;
+      this.passwordError = true;
+      // this.checkboxError = false;
     },
     eyeClick() {
-        if(this.eye === "mdi-eye-off") {
-            this.eye = "mdi-eye";
-            this.seePwd = "";
-        }
-        else {
-            this.eye = "mdi-eye-off";
-            this.seePwd = "password";
-        }
+      if (this.eye === "mdi-eye-off") {
+        this.eye = "mdi-eye";
+        this.seePwd = "";
+      } else {
+        this.eye = "mdi-eye-off";
+        this.seePwd = "password";
+      }
+    },
+    onSuccess() {
+      console.log("Success verity");
+      this.dialog = false;
+      document.location.href = "/work";
+    },
+    onFail() {
+      this.msg = "";
+    },
+    onRefresh() {
+      this.msg = "";
     }
   }
 };
--- src/application/sign/sign.js
+++ src/application/sign/sign.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './sign.vue'
 import router from './router' //引入 router
 import vuetify from '../../plugins/vuetify';
+import SlideVerify from 'vue-monoplasty-slide-verify';
+
+Vue.use(SlideVerify);
 
 Vue.config.productionTip = false
 
