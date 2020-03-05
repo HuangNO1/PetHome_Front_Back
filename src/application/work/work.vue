c1cf7d6ac493c6f79bc7c2ce5d51abfdd0afd421
Huang Po-Hsun <fh831.cp9gw@gmail.com>
2020-03-05 19:26:38 +0800

    初步調整work頁面應用布局

--- src/application/work/work.vue
+++ src/application/work/work.vue
@@ -1,11 +1,44 @@
 <template>
   <v-app>
-    <v-navigation-drawer app>
-      <!-- -->
+    <v-navigation-drawer
+      v-model="drawer"
+      :color="color"
+      :expand-on-hover="expandOnHover"
+      :mini-variant="miniVariant"
+      :right="right"
+      :src="bg"
+      absolute
+      app
+    >
+      <v-list dense nav class="py-0">
+        <v-list-item two-line :class="miniVariant && 'px-0'">
+          <v-list-item-avatar>
+            <img src="https://randomuser.me/api/portraits/men/81.jpg" />
+          </v-list-item-avatar>
+
+          <v-list-item-content>
+            <v-list-item-title>Application</v-list-item-title>
+            <v-list-item-subtitle>Subtext</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-divider></v-divider>
+
+        <v-list-item v-for="item in items" :key="item.title" link>
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
     </v-navigation-drawer>
 
     <v-app-bar app>
       <!-- -->
+      
     </v-app-bar>
 
     <!-- Sizes your content based upon application components -->
@@ -13,14 +46,37 @@
       <!-- Provides the application the proper gutter -->
       <v-container fluid>
         <!-- If using vue-router -->
-        work
+        <v-row justify="space-around">
+          <v-col cols="12">
+            <v-select v-model="color" :items="colors" label="Color"></v-select>
+          </v-col>
+
+          <v-switch v-model="drawer" class="ma-2" label="v-model"></v-switch>
+
+          <v-switch
+            v-model="miniVariant"
+            class="ma-2"
+            label="Mini variant"
+          ></v-switch>
+
+          <v-switch
+            v-model="expandOnHover"
+            class="ma-2"
+            label="Expand on hover"
+          ></v-switch>
+
+          <v-switch
+            v-model="background"
+            class="ma-2"
+            label="Background"
+          ></v-switch>
+
+          <v-switch v-model="right" class="ma-2" label="Right"></v-switch>
+        </v-row>
+
         <router-view></router-view>
       </v-container>
     </v-content>
-
-    <v-footer app>
-      <!-- -->
-    </v-footer>
   </v-app>
 </template>
 
@@ -34,6 +90,25 @@ export default {
 
   data: () => ({
     //
-  })
+    drawer: true,
+    items: [
+      { title: "Dashboard", icon: "mdi-view-dashboard" },
+      { title: "Photos", icon: "mdi-image" },
+      { title: "About", icon: "mdi-help-box" }
+    ],
+    color: "primary",
+    colors: ["primary", "blue", "success", "red", "teal"],
+    right: true,
+    miniVariant: false,
+    expandOnHover: false,
+    background: false
+  }),
+  computed: {
+    bg() {
+      return this.background
+        ? "https://cdn.vuetifyjs.com/images/backgrounds/bg-2.jpg"
+        : undefined;
+    }
+  }
 };
 </script>
