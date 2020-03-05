d1ff18a11d9d58aead4a1f1e6d52c86e83be50f0
Huang Po-Hsun <fh831.cp9gw@gmail.com>
2020-03-05 23:26:19 +0800

    work引入vuex

--- src/application/work/work.js
+++ src/application/work/work.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import App from './work.vue'
 import router from './router' //引入 router
 import vuetify from '../../plugins/vuetify';
+import Vuex from 'vuex'
 
+Vue.use(Vuex)
 Vue.config.productionTip = false
 
 new Vue({
