73b5b787f2faec92b7c0b632cd2160f4c0fcc90f
Huang Po-Hsun <fh831.cp9gw@gmail.com>
2020-04-30 23:47:25 +0800

    将VueX的managUser套入admin的Manager.vue

--- src/application/admin/page/Manage.vue
+++ src/application/admin/page/Manage.vue
@@ -2,7 +2,7 @@
   <div>
     <v-container fluid>
       <v-data-iterator
-        :items="items"
+        :items="users"
         :items-per-page.sync="itemsPerPage"
         :page="page"
         :search="search"
@@ -49,20 +49,26 @@
           <v-row>
             <v-col
               v-for="item in props.items"
-              :key="item.name"
+              :key="item.username"
               cols="12"
               sm="6"
               md="4"
-              lg="3"
+              lg="4"
             >
               <v-card>
-                <v-card-title class="subheading font-weight-bold">{{
-                  item.name
-                }}</v-card-title>
+                <v-card-title class="subheading font-weight-bold">
+                  <v-avatar size="36" style="margin-right: 1rem;">
+                    <img
+                      alt="Avatar"
+                      :src="item.avatar"
+                    />
+                  </v-avatar>
+                  {{ item.username }}
+                </v-card-title>
 
                 <v-divider></v-divider>
 
-                <v-list dense>
+                <v-list>
                   <v-list-item
                     v-for="(key, index) in filteredKeys"
                     :key="index"
@@ -135,7 +141,11 @@
 </template>
 
 <script>
+import { mapState, mapMutations } from "vuex";
+import { UPDATE_ALL_USERS_DETAIL } from "../store/mutations-types/manageUsers.js";
+
 export default {
+  created() {},
   data() {
     return {
       itemsPerPageArray: [4, 8, 12],
@@ -146,125 +156,28 @@ export default {
       itemsPerPage: 4,
       sortBy: "name",
       keys: [
-        "Name",
-        "Calories",
-        "Fat",
-        "Carbs",
-        "Protein",
-        "Sodium",
-        "Calcium",
-        "Iron",
-      ],
-      items: [
-        {
-          name: "Frozen Yogurt",
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
-          sodium: 87,
-          calcium: "14%",
-          iron: "1%",
-        },
-        {
-          name: "Ice cream sandwich",
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
-          sodium: 129,
-          calcium: "8%",
-          iron: "1%",
-        },
-        {
-          name: "Eclair",
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0,
-          sodium: 337,
-          calcium: "6%",
-          iron: "7%",
-        },
-        {
-          name: "Cupcake",
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3,
-          sodium: 413,
-          calcium: "3%",
-          iron: "8%",
-        },
-        {
-          name: "Gingerbread",
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9,
-          sodium: 327,
-          calcium: "7%",
-          iron: "16%",
-        },
-        {
-          name: "Jelly bean",
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0,
-          sodium: 50,
-          calcium: "0%",
-          iron: "0%",
-        },
-        {
-          name: "Lollipop",
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0,
-          sodium: 38,
-          calcium: "0%",
-          iron: "2%",
-        },
-        {
-          name: "Honeycomb",
-          calories: 408,
-          fat: 3.2,
-          carbs: 87,
-          protein: 6.5,
-          sodium: 562,
-          calcium: "0%",
-          iron: "45%",
-        },
-        {
-          name: "Donut",
-          calories: 452,
-          fat: 25.0,
-          carbs: 51,
-          protein: 4.9,
-          sodium: 326,
-          calcium: "2%",
-          iron: "22%",
-        },
-        {
-          name: "KitKat",
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7,
-          sodium: 54,
-          calcium: "12%",
-          iron: "6%",
-        },
+        "Username",
+        "Description",
+        "Email",
+        "Phone",
+        "Cash",
+        "Address",
+        "Password",
       ],
     };
   },
   computed: {
+    // get data from vuex
+    ...mapState({
+      users: (state) => {
+        return state.manageUsers.users;
+      },
+    }),
     numberOfPages() {
-      return Math.ceil(this.items.length / this.itemsPerPage);
+      return Math.ceil(this.users.length / this.itemsPerPage);
     },
     filteredKeys() {
-      return this.keys.filter((key) => key !== `Name`);
+      return this.keys.filter((key) => key !== `Username`);
     },
   },
   methods: {
